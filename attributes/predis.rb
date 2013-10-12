30e9df671e4c981ba196457e5b133b70f7637c2c
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-10-12 15:25:51 +0200

    switch loglevel of redis to notice;

--- attributes/predis.rb
+++ attributes/predis.rb
@@ -3,6 +3,15 @@
 # # # # #
 default['redisio']['mirror'] = "http://download.redis.io/releases"
 default['redisio']['version'] = '2.6.16'
+default['redisio']['servers'] = [
+  {
+    "name" => "master",
+    "port" => "6379",
+    "unixsocket" => "/var/run/redis/redis.sock",
+    "unixsocketperm" => "755",
+    "loglevel" => "notice"
+  }
+]
 
 # # # # # #
 # predis  #
