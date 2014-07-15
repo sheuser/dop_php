26706657eda1df47448cf450b58a0424c9e2887d
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-07-15 13:59:33 +0200

    add xdebug;

--- recipes/xdebug.rb
+++ recipes/xdebug.rb
@@ -9,21 +9,23 @@ include_recipe "build-essential"
 package = "xdebug"
 
 php_pear package do
-    if node['php']['xdebug']['version'] != "latest"
-        version "#{node['php']['xdebug']['version']}"
-    end
+    version "#{node['php']['xdebug']['version']}"
     #upgrade when package is installed and latest version is required
     action ( !(`pear list | grep #{package}`.empty?) and node['php']['xdebug']['version'] == "latest" ) ? :upgrade : :install
 end
 
-# copy over xdebug.ini to node
-template "#{node['php']['ext_conf_dir']}/xdebug.ini" do
-  source "zend_extension.ini.erb"
-  variables(:name => "xdebug", :directives => [])
+template "#{node['php']['fpm']['mods_dir']}/xdebug.ini" do
+  source "extension.ini.erb"
   owner "root"
   group "root"
-  mode 0644
+  mode "0644"
+  variables(:name => "xdebug", :directives => node['php']['xdebug']['ini'])
   not_if "php -m | grep xdebug"
+end
+
+link "#{node['php']['fpm']['conf_dir']}/20-xdebug.ini" do
+  to "#{node['php']['fpm']['mods_dir']}/xdebug.ini"
+  only_if node['php']['xdebug']['enabled']
 end
 
 file node['php']['xdebug']['remote_log'] do
