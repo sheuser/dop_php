0e904739c7f64b2f4fc45e14f7b234551c5f1423
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-12-16 19:03:56 +0100

    make pear installation idemponent (only install once)

--- recipes/_pear.rb
+++ recipes/_pear.rb
@@ -8,14 +8,14 @@ package 'expect'
 cookbook_file '/tmp/install-pear.sh' do
   source 'install-pear.sh'
   mode 0755
-  only_if { !File::exists?('/usr/bin/pear') }
+  only_if { !File.exist?('/usr/bin/pear') }
 end
 
 execute 'install_pear' do
   command '/tmp/install-pear.sh'
   action :run
   notifies :run, 'execute[remove pear installer]', :delayed
-  only_if { !File::exists?('/usr/bin/pear') }
+  only_if { !File.exist?('/usr/bin/pear') }
 end
 
 execute 'remove pear installer' do
