b6d1e398ab688a71b540c4bd3a4c7f8211784b5f
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-12-18 16:03:57 +0100

    explicitly set attributes in recipe

--- recipes/_php7.rb
+++ recipes/_php7.rb
@@ -2,9 +2,9 @@
 # Cookbook Name:: php
 # Recipe:: php7
 #
-node.default['php']['dir'] = '/etc/php'
-node.default['php']['conf_dir'] = '/etc/php/7.0/cli'
-node.default['php']['ext_conf_dir'] = '/etc/php/7.0/conf.d'
+node.set['php']['dir'] = '/etc/php'
+node.set['php']['conf_dir'] = '/etc/php/7.0/cli'
+node.set['php']['ext_conf_dir'] = '/etc/php/7.0/conf.d'
 
 # php7.0 seems to come with a already embedded mcrypt module,
 # so adding php7.0-mcrypt as package will lead to
@@ -23,17 +23,17 @@ node.set['php']['packages'] = %w(
   php7.0-opcache
   php7.0-pspell
 )
-node.default['php']['fpm']['service-name'] = 'php7.0-fpm'
-node.default['php']['fpm']['dir'] = "#{node['php']['dir']}/7.0/fpm"
-node.default['php']['fpm']['ext_conf_dir'] = "#{node['php']['fpm']['dir']}/conf.d"
-node.default['php']['fpm']['pool_dir'] = "#{node['php']['fpm']['dir']}/pool.d"
-node.default['php']['fpm']['log_dir'] = "/var/log/#{node['php']['fpm']['service-name']}"
-node.default['php']['fpm']['pidfile'] = "/var/run/#{node['php']['fpm']['service-name']}.pid"
-node.default['php']['fpm']['logfile'] = "/var/log/#{node['php']['fpm']['service-name']}/fpm-master.log"
-node.default['php']['fpm']['rotfile'] = "/etc/logrotate.d/#{node['php']['fpm']['service-name']}"
-node.default['php']['fpm']['mods_dir'] = "#{node['php']['dir']}/mods-available"
-node.default['php']['fpm']['log_dir'] = "/var/log/#{node['php']['fpm']['service-name']}"
-node.default['php']['fpm']['rotfile'] = "/etc/logrotate.d/#{node['php']['fpm']['service-name']}"
-node.default['php']['ini']['directives']['fpm'] = node['php']['fpm']['conf']
-node.default['php']['fpm']['conf']['global']['error_log'] = "#{node['php']['fpm']['log_dir']}/#{node['php']['fpm']['service-name']}.log"
-node.default['php']['fpm']['workers']['slow_filename'] = "#{node['php']['fpm']['log_dir']}/#{node['php']['fpm']['service-name']}-slow.log"
+node.set['php']['fpm']['service-name'] = 'php7.0-fpm'
+node.set['php']['fpm']['dir'] = "#{node['php']['dir']}/7.0/fpm"
+node.set['php']['fpm']['ext_conf_dir'] = "#{node['php']['fpm']['dir']}/conf.d"
+node.set['php']['fpm']['pool_dir'] = "#{node['php']['fpm']['dir']}/pool.d"
+node.set['php']['fpm']['log_dir'] = "/var/log/#{node['php']['fpm']['service-name']}"
+node.set['php']['fpm']['pidfile'] = "/var/run/#{node['php']['fpm']['service-name']}.pid"
+node.set['php']['fpm']['logfile'] = "/var/log/#{node['php']['fpm']['service-name']}/fpm-master.log"
+node.set['php']['fpm']['rotfile'] = "/etc/logrotate.d/#{node['php']['fpm']['service-name']}"
+node.set['php']['fpm']['mods_dir'] = "#{node['php']['dir']}/mods-available"
+node.set['php']['fpm']['log_dir'] = "/var/log/#{node['php']['fpm']['service-name']}"
+node.set['php']['fpm']['rotfile'] = "/etc/logrotate.d/#{node['php']['fpm']['service-name']}"
+node.set['php']['ini']['directives']['fpm'] = node['php']['fpm']['conf']
+node.set['php']['fpm']['conf']['global']['error_log'] = "#{node['php']['fpm']['log_dir']}/#{node['php']['fpm']['service-name']}.log"
+node.set['php']['fpm']['workers']['slow_filename'] = "#{node['php']['fpm']['log_dir']}/#{node['php']['fpm']['service-name']}-slow.log"
