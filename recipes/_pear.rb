bb085b2b0e66fb1bb09626d08c68109bac41da1c
Sýlvan Heuser <Sylvan.Heuser@flexoptix.net>
2016-06-30 14:05:22 +0200

    Quick workaround for go-pear installation trashing system-installed pear

--- recipes/_pear.rb
+++ recipes/_pear.rb
@@ -11,15 +11,18 @@ src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
 remote_file src_filepath do
   source node['php']['pear']['download_url']
   checksum node['php']['pear']['checksum']
+  only_if { !File.exist?('/usr/bin/pear') }
 end
 
 cookbook_file "#{Chef::Config['file_cache_path']}/install-pear.sh" do
   source 'install-pear.sh'
   mode 0755
+  only_if { !File.exist?('/usr/bin/pear') }
 end
 
 execute 'install_pear' do
   cwd Chef::Config['file_cache_path']
   command "#{Chef::Config['file_cache_path']}/install-pear.sh"
   action :run
+  only_if { !File.exist?('/usr/bin/pear') }
 end
